d35664d7587d1b1a6abed17db3fd7a9b14e9e560
ilya <ilya@alk0.info>
2026-09-21 21:32:41 +0300

    Исправление: вынес вывод массива в отдельную функцию print_array

--- hw-2/simple_implementation/main.cpp
+++ hw-2/simple_implementation/main.cpp
@@ -1,50 +1,45 @@
 #include <iostream>
 
 void my_sort(int *arr, const int size);
+void print_array(const char* comment, int* arr, const int size);
 
-// TODO Интерфейс пользователя должен быть на русском языке
 int main() {
-    // TODO Пользователь вводит размер массив и элементы массива
-	int size;
-	std::cout << "Введите размер массива: ";
-	std::cin >> size;
-	int* arr = new int[size];
-	std::cout << "Введите " << size << " элементов массива:\n";
-	for (int i = 0; i < size; i++) {
-		std::cin >> arr[i];
-	}
-
-	// TODO вызвается void my_sort(int *arr, int size)
-	// TODO Выводится первоначальный массив и отсортированный
-	std::cout << "Исходный массив: ";
-	for (int i = 0; i < size; i++) {
-		std::cout << arr[i] << " ";
-	}
-	std::cout << "\n";
-
-	my_sort(arr, size);
-
-	std::cout << "Отсортированный массив: ";
-	for (int i = 0; i < size; i++) {
-		std::cout << arr[i] << " ";
-	}
-	std::cout << "\n";
-
-	// Хорошая практика: освободить память, которую мы выделили через new
-    delete[] arr;
+    int size;
+    std::cout << "Введите размер массива: ";
+    std::cin >> size;
     
+    int* arr = new int[size];
+    std::cout << "Введите " << size << " элементов массива:\n";
+    for (int i = 0; i < size; i++) {
+        std::cin >> arr[i];
+    }
+
+    print_array("Исходный массив:", arr, size);
+
+    my_sort(arr, size);
+
+    print_array("Отсортированный массив:", arr, size);
+    
+    delete[] arr;
     return 0;
 }
 
+void print_array(const char* comment, int* arr, const int size) {
+    std::cout << comment << " ";
+    for (int i = 0; i < size; i++) {
+        std::cout << arr[i] << " ";
+    }
+    std::cout << "\n";
+}
+
 void my_sort(int *arr, const int size) {
-	// TODO
-	for (int i = 0; i < size - 1; i++) {
-		for (int j = 0; j < size - 1 - i; j++) {
-			if (arr[j] > arr[j + 1]) {
-				int temp = arr[j];
-				arr[j] = arr[j + 1];
-				arr[j + 1] = temp;
-			}
-		}
-	}
+    for (int i = 0; i < size - 1; i++) {
+        for (int j = 0; j < size - 1 - i; j++) {
+            if (arr[j] > arr[j + 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
 }
